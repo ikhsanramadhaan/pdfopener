f1ae9298cab6d9873607a2f8965b19dd9c86db49
ikhsanramadhaan <ikhsanramadhan28@gmail.com>
2019-06-23 16:15:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int PICK_PDF_REQUEST = 1;
+    private static final int STORAGE_PERMISSION_CODE = 123;
+
+    private Button btn_choose;
+    private TextView tv_result;
+    private Uri filePath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_choose = findViewById(R.id.choose_pdf);
+        tv_result = findViewById(R.id.result);
+        requestStoragePermission();
+
+        btn_choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("application/pdf");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Pdf"), PICK_PDF_REQUEST);
+            }
+        });
+    }
+//    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+//    public void uploadMultipart() {
+//        //getting name for the image
+//        String name = tv_result.getText().toString().trim();
+//
+//        //getting the actual path of the image
+//        String path = FilePath.getPath(this, filePath);
+//
+//        if (path == null) {
+//
+//            Toast.makeText(this, "Please move your .pdf file to internal storage and retry", Toast.LENGTH_LONG).show();
+//        } else {
+//            //Uploading code
+//            try {
+//                String uploadId = UUID.randomUUID().toString();
+//
+//                //Creating a multi part request
+//                new MultipartUploadRequest(this, uploadId, UPLOAD_URL)
+//                        .addFileToUpload(path, "pdf") //Adding file
+//                        .addParameter("name", name) //Adding text parameter to the request
+//                        .setNotificationConfig(new UploadNotificationConfig())
+//                        .setMaxRetries(2)
+//                        .startUpload(); //Starting the upload
+//
+//            } catch (Exception exc) {
+//                Toast.makeText(this, exc.getMessage(), Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_PDF_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+        }
+    }
+
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            return;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+        }
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        //Checking the request code of our request
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+
+            //If permission is granted
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Displaying a toast
+                Toast.makeText(this, "Permission granted now you can read the storage", Toast.LENGTH_LONG).show();
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(this, "Oops you just denied the permission", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
